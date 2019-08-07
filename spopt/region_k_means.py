98cb1c2c705ccc3e561888df2a0d1f1003a1336e
Serge Rey <sjsrey@gmail.com>
2019-08-07 14:44:01 -0700

    ENH: region-k-means

--- spopt/region_k_means.py
+++ spopt/region_k_means.py
@@ -46,7 +46,7 @@ def region_neighbors(a_list, region):
     return [j for j in neighbors if j not in region]
 
 
-def _centroids(regions, data):
+def _centroid(regions, data):
     """Get centroids for all regions"""
     return numpy.array([data[region, :].mean(axis=0) for region in regions])
 
@@ -73,6 +73,8 @@ def is_neighbor(area, region, w):
 def region_k_means(X, n_clusters, w):
     """Region-K-means
 
+    K-means with the constraint that each cluster forms a spatially connected component.
+
 
     Parameters
     ----------
@@ -94,9 +96,79 @@ def region_k_means(X, n_clusters, w):
 
     centroid: float ndarray with shape (k, n_features)
        Centroids found at the last iteration of region_k_means.
-       
+
+    iters: int
+          number of iterations for reassignment phase
 
     """
+
+    data = X
+    a_list = w.to_adjlist(remove_symmetric=False)
+    areas = numpy.arange(w.n).astype(int)
+    k = n_clusters
+    seeds = _seeds(areas, k)
+
+    # initial assignment phase
+    label = numpy.array([-1] * w.n).astype(int)
+    for i, seed in enumerate(seeds):
+        label[seed] = i
+    to_assign = areas[label == -1]
+    c = 0
+    while to_assign.size > 0:
+        assignments = defaultdict(list)
+        for rid in range(k):
+            region = areas[label == rid]
+            neighbors = region_neighbors(a_list, region)
+            neighbors = [j for j in neighbors if j in to_assign]
+            if neighbors:
+                d_min = numpy.inf
+                centroid = data[region].mean(axis=0)
+                for neighbor in neighbors:
+                    d = ((data[neighbor] - centroid) ** 2).sum()
+                    if d < d_min:
+                        idx = neighbor
+                        d_min = d
+                assignments[idx].append([rid, d_min])
+        for key in assignments:
+            assignment = assignments[key]
+            if len(assignment) == 1:
+                r, d = assignment[0]
+                label[key] = r
+            else:
+                d_min = numpy.inf
+                for match in assignment:
+                    r, d = match
+                    if d < d_min:
+                        idx = r
+                        d_min = d
+                label[key] = idx
+
+        to_assign = areas[label == -1]
+
+    # reassignment phase
+    changed = []
+    g = w_to_g(w)
+
+    iters = 1
+
+    # want to loop this until candidates is empty
+    regions = [areas[label == r].tolist() for r in range(k)]
+    centroid = _centroid(regions, data)
+    closest = numpy.array(_closest(data, centroid))
+    candidates = areas[closest != label]
+    candidates = ok_moves(candidates, regions, label, closest, g, w, areas)
+    while candidates:
+        # make moves
+        for area in candidates:
+            label[area] = closest[area]
+        regions = [areas[label == r].tolist() for r in range(k)]
+        centroid = _centroid(regions, data)
+        closest = numpy.array(_closest(data, centroid))
+        candidates = areas[closest != label]
+        candidates = ok_moves(candidates, regions, label, closest, g, w, areas)
+        iters += 1
+
+    return centroid, label, iters
 
 
 class RegionKMeansHeuristic(BaseSpOptHeuristicSolver):
@@ -106,59 +178,7 @@ class RegionKMeansHeuristic(BaseSpOptHeuristicSolver):
         self.k = k
 
     def solve(self):
-        w = self.w
-        data = self.data
-        a_list = w.to_adjlist(remove_symmetric=False)
-        areas = numpy.arange(w.n).astype(int)
-        k = self.k
-        seeds = _seeds(areas, k)
-
-        # initial assignment phase
-        labels_ = numpy.array([-1] * w.n).astype(int)
-        for i, seed in enumerate(seeds):
-            labels_[seed] = i
-        to_assign = areas[labels_ == -1]
-        c = 0
-        while to_assign.size > 0:
-            assignments = defaultdict(list)
-            for rid in range(k):
-                region = areas[labels_ == rid]
-                neighbors = region_neighbors(a_list, region)
-                neighbors = [j for j in neighbors if j in to_assign]
-                if neighbors:
-                    d_min = numpy.inf
-                    centroid = data[region].mean(axis=0)
-                    for neighbor in neighbors:
-                        d = ((data[neighbor] - centroid) ** 2).sum()
-                        if d < d_min:
-                            idx = neighbor
-                            d_min = d
-                    assignments[idx].append([rid, d_min])
-            for key in assignments:
-                assignment = assignments[key]
-                if len(assignment) == 1:
-                    r, d = assignment[0]
-                    labels_[key] = r
-                else:
-                    d_min = numpy.inf
-                    for match in assignment:
-                        r, d = match
-                        if d < d_min:
-                            idx = r
-                            d_min = d
-                    labels_[key] = idx
-
-            to_assign = areas[labels_ == -1]
-
-        # swapping phase
-        changed = []
-        g = w_to_g(w)
-
-        # want to loop this until candidates is empty
-        regions = [areas[labels_ == r].tolist() for r in range(k)]
-        centroids = _centroids(regions, data)
-        closest = numpy.array(_closest(data, centroids))
-        candidates = areas[closest != labels_]
-        candidates = ok_moves(candidates, regions, labels_, closest, g, w, areas)
-
-        self.labels_ = labels_
+        centroid, label, iters = region_k_means(self.data, self.k, self.w)
+        self.labels_ = label
+        self.centroids_ = centroid
+        self.iters_ = iters
