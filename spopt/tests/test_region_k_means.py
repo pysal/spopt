d608a4d250bf49c351d126ac757aa07e15403e15
Serge Rey <sjsrey@gmail.com>
2019-11-05 17:58:54 -0800

    Dev: building tests

---
+++ spopt/tests/test_region_k_means.py
@@ -0,0 +1,16 @@
+import numpy
+import libpysal
+import os
+import sys
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
+from ..region_k_means import RegionKMeansHeuristic
+
+
+def test_RegionKMeansHeuristic():
+    numpy.random.seed(12345)
+    w = libpysal.weights.lat2W(3,3)
+    data = numpy.random.normal(size=(w.n,3))
+    RKM = RegionKMeansHeuristic
+    model = RKM(data, 3, w)
+    model.solve()
+    numpy.array_equal(model.labels_, numpy.array([1, 2, 2, 1, 2, 0, 1, 1, 0]))
