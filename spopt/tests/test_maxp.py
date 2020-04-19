279f0fde1a4a9118fafb5491afc69fcad17029b1
Serge Rey <sjsrey@gmail.com>
2020-04-19 12:45:23 -0700

    ENH: tests for maxp and additional drafting for notebook

--- spopt/tests/test_maxp.py
+++ spopt/tests/test_maxp.py
@@ -8,14 +8,19 @@ from .. import MaxPHeuristic
 
 
 def test_MaxPHeuristic():
-    numpy.random.seed(12345)
-    gdf = gpd.read_file('../data/n100.shp')
-    w = libpysal.weights.Rook.from_dataframe(gdf)
-    attrs_name = ['SAR1']
-    threshold_name = 'Uniform2'
-    threshold = 100
-    top_n = 2
-    model = MaxPHeuristic(gdf, w, attrs_name, threshold_name, threshold, top_n)
-    model.solve()
-    numpy.array_equal(model.labels_, numpy.array([1, 2, 2, 1, 2, 0, 1, 1, 0]))
 
+    pth = libpysal.examples.get_path('mexicojoin.shp')
+    mexico = gpd.read_file(pth)
+    mexico['count'] = 1
+    w = libpysal.weights.Queen.from_dataframe(mexico)
+    attrs_name = [f'PCGDP{year}' for year in range(1950, 2010, 10)]
+    threshold = 4
+    top_n = 2
+    threshold_name = 'count'
+    numpy.random.seed(123456)
+    model = MaxPHeuristic(mexico, w, attrs_name, threshold_name,
+                          threshold, top_n)
+    model.solve()
+    labels = [5, 5, 3, 6, 6, 6, 1, 1, 1, 1, 8, 6, 8, 2, 2, 8, 2, 8, 7, 7, 2,
+              7, 5, 3, 3, 5, 3, 4, 4, 4, 4, 7]
+    numpy.array_equal(model.labels_, labels)
