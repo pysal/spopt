8c173628b392d67526f7eb9d9822cd89c6a5aaa2
eli knaap <ek@knaaptime.com>
2021-01-12 12:04:06 -0800

    conditional import spopt

--- spopt/BaseClass.py
+++ spopt/BaseClass.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from ortools.linear_solver import pywraplp
 
 
 class BaseSpOptSolver(ABC):
@@ -9,10 +8,7 @@ class BaseSpOptSolver(ABC):
 
     @abstractmethod
     def solve(self):
-        """Solve the optimization model.
-
-        """
-
+        """Solve the optimization model."""
         pass
 
 
@@ -21,45 +17,45 @@ class BaseSpOptExactSolver(BaseSpOptSolver):
 
     Attributes
     ----------
-    
+
     spOptSolver : pywraplp.Solver
         The or-tools MIP solver.
 
     """
 
     def __init__(self, name):
-        """
-        
+        """Initialize.
+
         Parameters
         ----------
-        
         name : str
             The desired name for the model.
-        
         """
-        self.spOptSolver = pywraplp.Solver(
-            name, pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING
-        )
+        try:
+            from ortools.linear_solver import pywraplp
+
+            self.spOptSolver = pywraplp.Solver(
+                name, pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING
+            )
+        except ImportError:
+            raise ImportError(
+                "ortools is a requirement for exact solvers. "
+                "you can install it with `pip install ortools`"
+            )
+
+        self.name = name
 
     def solve(self):
-        """Solve the optimization model.
-
-        """
-
+        """Solve the optimization model."""
         self.spOptSolver.Solve()
 
 
 class BaseSpOptHeuristicSolver(BaseSpOptSolver):
-    """Base class for all spatial optimization model heuristic solvers.
-
-    """
+    """Base class for all spatial optimization model heuristic solvers."""
 
     @abstractmethod
     def solve(self):
-        """Solve the optimization model.
-
-        """
-
+        """Solve the optimization model."""
         pass
 
 
