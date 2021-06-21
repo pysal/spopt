4148710a4ec859e4c332889ca2046bbba1542d6b
James Gaboardi <jgaboardi@gmail.com>
2021-06-21 14:36:37 -0400

    blacken ward, clean up tests

--- spopt/region/ward.py
+++ spopt/region/ward.py
@@ -8,85 +8,87 @@ from sklearn.cluster import (
     SpectralClustering,
 )
 
+
 class WardSpatial(BaseSpOptHeuristicSolver):
-    """ Agglomerative clustering using Ward linkage with a spatial connectivity constraint.
-        
-        Parameters
-        ----------
-        
-        gdf : geopandas.GeoDataFrame, required
-            Geodataframe containing original data
+    """Agglomerative clustering using Ward linkage with a spatial connectivity constraint.
 
-        w : libpysal.weights.W, required
-            Weights object created from given data
+    Parameters
+    ----------
 
-        attrs_name : list, required
-            Strings for attribute names (cols of ``geopandas.GeoDataFrame``).
+    gdf : geopandas.GeoDataFrame, required
+        Geodataframe containing original data
 
-        n_clusters : int, optional, default: 5
-            The number of clusters to form.
+    w : libpysal.weights.W, required
+        Weights object created from given data
 
-        clustering_kwds: dictionary, optional, default: dict()
-            Other parameters about clustering could be used in sklearn.cluster.AgglometariveClustering.
-        
-        Returns
-        -------
+    attrs_name : list, required
+        Strings for attribute names (cols of ``geopandas.GeoDataFrame``).
 
-        labels_ : numpy.array
-            Cluster labels for observations.
+    n_clusters : int, optional, default: 5
+        The number of clusters to form.
+
+    clustering_kwds: dictionary, optional, default: dict()
+        Other parameters about clustering could be used in sklearn.cluster.AgglometariveClustering.
+
+    Returns
+    -------
+
+    labels_ : numpy.array
+        Cluster labels for observations.
 
 
-        Examples
-        --------
+    Examples
+    --------
 
-        >>> import numpy as np
-        >>> import libpysal
-        >>> import geopandas as gpd
-        >>> from spopt.region import WardSpatial
+    >>> import numpy as np
+    >>> import libpysal
+    >>> import geopandas as gpd
+    >>> from spopt.region import WardSpatial
 
-        Read the data.
+    Read the data.
 
-        >>> pth = libpysal.examples.get_path('airbnb_Chicago 2015.shp')
-        >>> chicago = gpd.read_file(pth)
+    >>> pth = libpysal.examples.get_path('airbnb_Chicago 2015.shp')
+    >>> chicago = gpd.read_file(pth)
 
-        Initialize the parameters.
+    Initialize the parameters.
 
-        >>> w = libpysal.weights.Queen.from_dataframe(chicago)
-        >>> attrs_name = ['num_spots']
-        >>> n_clusters = 8
+    >>> w = libpysal.weights.Queen.from_dataframe(chicago)
+    >>> attrs_name = ['num_spots']
+    >>> n_clusters = 8
 
-        Run the skater algorithm.
+    Run the skater algorithm.
 
-        >>> model = WardSpatial(chicago, w, attrs_name, n_clusters)
-        >>> model.solve()
+    >>> model = WardSpatial(chicago, w, attrs_name, n_clusters)
+    >>> model.solve()
 
-        Get the region IDs for unit areas.
+    Get the region IDs for unit areas.
 
-        >>> model.labels_
-        
-        Show the clustering results.
+    >>> model.labels_
 
-        >>> chicago['ward_new'] = model.labels_
-        >>> chicago.plot(column='ward_new', categorical=True, figsize=(12,8), edgecolor='w')
+    Show the clustering results.
 
-        """
+    >>> chicago['ward_new'] = model.labels_
+    >>> chicago.plot(column='ward_new', categorical=True, figsize=(12,8), edgecolor='w')
 
+    """
 
     def __init__(self, gdf, w, attrs_name, n_clusters=5, clustering_kwds=dict()):
-        
+
         self.gdf = gdf
         self.w = w
         self.attrs_name = attrs_name
         self.n_clusters = n_clusters
         self.clustering_kwds = clustering_kwds
 
-
     def solve(self):
         """Solve the Ward"""
         data = self.gdf
         X = data[self.attrs_name].values
         model = AgglomerativeClustering(
-            n_clusters=self.n_clusters, connectivity=self.w.sparse, linkage="ward", **self.clustering_kwds
+            n_clusters=self.n_clusters,
+            connectivity=self.w.sparse,
+            linkage="ward",
+            **self.clustering_kwds
         )
         model.fit(X)
         self.labels_ = model.labels_
--- spopt/tests/test_ward.py
+++ spopt/tests/test_ward.py
@@ -1,59 +1,47 @@
-import numpy as np
+import geopandas
 import libpysal
-import geopandas as gpd
-import os
-import sys
-from spopt.region import WardSpatial
+import numpy
 import unittest
+
+from spopt.region import WardSpatial
+
+
+# Empirical tests -- Mexican states
+RANDOM_STATE = 12345
+pth = libpysal.examples.get_path("mexicojoin.shp")
+MEXICO = geopandas.read_file(pth)
 
 
 class TestWard(unittest.TestCase):
     def setUp(self):
-        self.pth = libpysal.examples.get_path("mexicojoin.shp")
-        self.mexico = gpd.read_file(self.pth)
+
+        self.mexico = MEXICO.copy()
         self.mexico["count"] = 1
         self.attrs_name = [f"PCGDP{year}" for year in range(1950, 2010, 10)]
         self.w = libpysal.weights.Queen.from_dataframe(self.mexico)
-        np.random.seed(123456)
-        self.model = WardSpatial(gdf=self.mexico, w=self.w, attrs_name=self.attrs_name)
-        self.model.solve()
+        self.known_labels = [
+            2,
+            2,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            3,
+            0,
+            0,
+            1,
+            4,
+            0,
+        ]
+        self.known_labels += [1, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 2, 2, 0]
 
     def test_labels_(self):
-        observed_labels = self.model.labels_
-        known_labels = np.array(
-            [
-                2,
-                2,
-                0,
-                0,
-                0,
-                0,
-                0,
-                0,
-                0,
-                0,
-                3,
-                0,
-                0,
-                1,
-                4,
-                0,
-                1,
-                0,
-                0,
-                0,
-                0,
-                0,
-                2,
-                2,
-                2,
-                0,
-                0,
-                0,
-                0,
-                2,
-                2,
-                0,
-            ]
-        )
-        np.testing.assert_equal(observed_labels, known_labels)
+        numpy.random.seed(RANDOM_STATE)
+        model = WardSpatial(gdf=self.mexico, w=self.w, attrs_name=self.attrs_name)
+        model.solve()
+
+        numpy.testing.assert_equal(model.labels_, self.known_labels)
