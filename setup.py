59eb2a8bcd69347b3ada2c0e96a1bec01f56d4b6
James Gaboardi <jgaboardi@gmail.com>
2021-12-23 15:04:48 -0500

    add 310 to CI matrix

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ def setup_package():
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
         ],
         license="3-Clause BSD",
         packages=find_packages(),
