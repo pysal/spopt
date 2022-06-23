d7fd196d29a66c7a28f5e81f41da4514872d4438
Erin Olson <rosemary.erin.o@gmail.com>
2022-06-23 11:56:48 -0500

    add LSCPB

--- spopt/locate/__init__.py
+++ spopt/locate/__init__.py
@@ -1,5 +1,5 @@
 from .base import BaseOutputMixin, CoveragePercentageMixin, MeanDistanceMixin
-from .coverage import LSCP, MCLP
+from .coverage import LSCP, MCLP, LSCPB
 from .p_median import PMedian
 from .p_center import PCenter
 from .util import simulated_geo_points
