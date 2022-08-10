f4919b2321f0be6533c44b23b4438464e586d183
Erin Olson <rosemary.erin.o@gmail.com>
2022-08-10 11:52:17 -0400

    reference new p_dispersion.py file

--- spopt/locate/__init__.py
+++ spopt/locate/__init__.py
@@ -1,6 +1,6 @@
 from .base import BaseOutputMixin, CoveragePercentageMixin, MeanDistanceMixin
 from .coverage import LSCP, MCLP, LSCPB
-from .disperse import PDispersion
+from .p_dispersion import PDispersion
 from .p_median import PMedian
 from .p_center import PCenter
 from .util import simulated_geo_points
