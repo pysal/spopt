24602b45a8d7a1053c33ee50e35a41d2a603aebb
James Gaboardi <jgaboardi@gmail.com>
2022-10-29 13:37:19 -0400

    resolves #291 #290 #284

--- setup.py
+++ setup.py
@@ -54,7 +54,6 @@ def setup_package():
             "Topic :: Scientific/Engineering :: GIS",
             "License :: OSI Approved :: BSD License",
             "Programming Language :: Python",
-            "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
