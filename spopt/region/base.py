6d44416f615ca911a70d1587d65f9b1f453bed9e
James Gaboardi <jgaboardi@gmail.com>
2022-11-03 11:03:52 -0400

    add RegionMixin back

--- spopt/region/base.py
+++ spopt/region/base.py
@@ -7,6 +7,30 @@ import networkx
 from scipy.spatial import KDTree
 
 
+class RegionMixin(object):
+    """Mixin class for all region solvers."""
+
+    _solver_type = "regionalizer"
+
+    def solve_assign(self, X, adjacency):
+        """
+        Parameters
+        ----------
+        X :
+            ...
+        adjacency :
+            ...
+        Returns
+        -------
+        _labels_ :
+            ...
+        """
+
+        self.solve(X, adjacency)
+        _labels_ = self.labels_
+        return _labels_
+
+
 def w_to_g(w):
     """Get a ``networkx`` graph from a PySAL W.
 
