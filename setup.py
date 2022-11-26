e312703f63fd7d2233a1a9bf6d3759d42036b91d
James Gaboardi <jgaboardi@gmail.com>
2022-11-26 13:40:01 -0500

    fix cascading doc references

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ def setup_package():
         install_requires=install_reqs,
         extras_require=extras_reqs,
         zip_safe=False,
-        python_requires=">=3.7",
+        python_requires=">=3.8",
     )
 
 
