af60345e432e29f2263a1986cef150cee9951ec4
James Gaboardi <jgaboardi@gmail.com>
2022-11-26 13:50:20 -0500

    py311 testing

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ def setup_package():
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
         ],
         license="3-Clause BSD",
         packages=find_packages(),
