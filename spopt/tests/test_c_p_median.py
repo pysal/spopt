6b9b7bf298a4bd63991329267fb58b25edc68228
rongboxu <rongbo.xu.21@ucl.ac.uk>
2023-05-30 13:59:39 +0800

    add the synthetic case for capacitated p median test

--- spopt/tests/test_c_p_median.py
+++ spopt/tests/test_c_p_median.py
@@ -1,11 +1,108 @@
 from spopt.locate.base import FacilityModelBuilder, LocateSolver, T_FacModel,SpecificationError
 import numpy
 import pandas
+import geopandas
 import pulp
+import spaghetti
 
 from spopt.locate import PMedian
+from spopt.locate.util import simulated_geo_points
 import pytest
 import os
+
+class TestSyntheticLocate:
+    def setup_method(self) -> None:
+        self.dirpath = os.path.join(os.path.dirname(__file__), "./data/")
+
+        lattice = spaghetti.regular_lattice((0, 0, 10, 10), 9, exterior=True)
+        ntw = spaghetti.Network(in_data=lattice)
+        gdf = spaghetti.element_as_gdf(ntw, arcs=True)
+        street = geopandas.GeoDataFrame(
+            geopandas.GeoSeries(gdf["geometry"].buffer(0.2).unary_union),
+            crs=gdf.crs,
+            columns=["geometry"],
+        )
+
+        client_count = 2
+        facility_count = 3
+
+        self.client_points = simulated_geo_points(street, needed=client_count, seed=5)
+        self.facility_points = simulated_geo_points(street, needed=facility_count, seed=6)
+
+        ntw = spaghetti.Network(in_data=lattice)
+
+        ntw.snapobservations(self.client_points, "clients", attribute=True)
+        ntw.snapobservations(self.facility_points, "facilities", attribute=True)
+
+        self.clients_snapped = spaghetti.element_as_gdf(
+            ntw, pp_name="clients", snapped=True
+        )
+
+        self.facilities_snapped = spaghetti.element_as_gdf(
+            ntw, pp_name="facilities", snapped=True
+        )
+
+        self.cost_matrix = ntw.allneighbordistances(
+            sourcepattern=ntw.pointpatterns["clients"],
+            destpattern=ntw.pointpatterns["facilities"],
+        )
+
+    def test_c_p_median_from_cost_matrix(self):
+        facility_capacity = numpy.array([5, 7,10])
+        demand_quantity = numpy.array([4, 10])
+        p_median = PMedian.from_cost_matrix(
+            self.cost_matrix, 
+            demand_quantity, 
+            p_facilities=2,
+            facility_capacities=facility_capacity)
+        result = p_median.solve(pulp.PULP_CBC_CMD(msg=False))
+        assert isinstance(result, PMedian)
+
+        known = [[1], [2]]
+        observed = result.cli2fac
+        assert known == observed
+
+        known = [[], [0], [1]]
+        observed = result.fac2cli
+        assert known == observed
+
+    
+    def test_c_p_median_with_predefined_facilities_from_cost_matrix(self):
+        facility_capacity = numpy.array([5, 7,10])
+        demand_quantity = numpy.array([4, 10])
+        predefine = numpy.array([2])
+        p_median = PMedian.from_cost_matrix(
+            self.cost_matrix, 
+            demand_quantity, 
+            p_facilities=2,
+            facility_capacities=facility_capacity,
+            predefined_facilities_arr=predefine,
+            fulfill_predefined_fac=True)
+        result = p_median.solve(pulp.PULP_CBC_CMD(msg=False))
+        assert isinstance(result, PMedian)
+
+        known = [[1], [2]]
+        observed = result.cli2fac
+        assert known == observed
+
+        known = [[], [0], [1]]
+        observed = result.fac2cli
+        assert known == observed
+
+    def test_c_p_median_with_predefined_facilities_infeasible(self):
+        facility_capacity = numpy.array([5, 7,10])
+        demand_quantity = numpy.array([4, 10])
+        predefine = numpy.array([0])
+        p_median = PMedian.from_cost_matrix(
+            self.cost_matrix, 
+            demand_quantity, 
+            p_facilities=2,
+            facility_capacities=facility_capacity,
+            predefined_facilities_arr=predefine,
+            fulfill_predefined_fac=True)
+        with pytest.raises(RuntimeError, match="Model is not solved: Infeasible."):
+            p_median.solve(pulp.PULP_CBC_CMD(msg=False))
+
 
 class TestRealWorldLocate:
     def setup_method(self) -> None:
